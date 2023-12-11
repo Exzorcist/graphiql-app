17f118338ce826e051e6617427a1b911b3bcf9c3
Ivan K <ivan.karpovich.frontend@gmail.com>
2023-12-11 21:20:13 +0300

    refactor: update code using routing information

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,12 @@
+import SignIn from '../components/Login/SignIn';
+
 function Login() {
-  return <div>This is a Login page</div>;
+  return (
+    <>
+      <div>This is a Login page</div>
+      <SignIn />
+    </>
+  );
 }
 
 export default Login;
--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,5 +1,12 @@
+import SignUp from '../components/Login/SignUp';
+
 function Registration() {
-  return <div>This is a Registration page</div>;
+  return (
+    <>
+      <div>This is a Registration page</div>
+      <SignUp />
+    </>
+  );
 }
 
 export default Registration;
