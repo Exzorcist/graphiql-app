33affa69bbaec8f1d355fddaea20a411ea591ab2
Ivan K <ivan.karpovich.frontend@gmail.com>
2023-12-12 21:02:35 +0300

    refactor: restore throw block to provider

--- src/provider/LocalizationProvider.tsx
+++ src/provider/LocalizationProvider.tsx
@@ -2,6 +2,14 @@ import { createContext, useContext } from 'react';
 import { ILocalizationProvider } from '@/types/Provider';
 
 const LocalizationContext = createContext<ILocalizationProvider | null>(null);
-
 export const LocalizationProvider = LocalizationContext.Provider;
-export const useLocalizationtContext = () => useContext(LocalizationContext);
+
+export const useLocalizationtContext = () => {
+  const data = useContext(LocalizationContext);
+
+  if (!data) {
+    throw new Error('Can not "useLocalizationtContext" outside of the "LocalizationProvider"');
+  } else {
+    return data;
+  }
+};
