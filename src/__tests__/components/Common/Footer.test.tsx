0bed7b23ce2d6ed2ff5efb4bdb62e28295de7a15
Полина <polina_kazhina@mail.ru>
2023-12-20 18:01:13 +0300

    fix: fix test for pushing

--- src/__tests__/components/Common/Footer.test.tsx
+++ src/__tests__/components/Common/Footer.test.tsx
@@ -1,8 +1,17 @@
 import { screen, render } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import Footer from '@/components/Common/Footer';
+import { store } from '@/redux/store';
+import LocalizationProvider from '@/providers/LocalizationProvider';
 
 test('[ Footer ]: base test', async () => {
-  render(<Footer />);
+  render(
+    <Provider store={store}>
+      <LocalizationProvider>
+        <Footer />
+      </LocalizationProvider>
+    </Provider>
+  );
 
   expect(screen.getByText('"Just Do It" team')).toBeInTheDocument();
   expect(screen.getByText('2023')).toBeInTheDocument();
