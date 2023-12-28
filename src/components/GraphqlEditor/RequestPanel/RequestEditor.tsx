0d242d0ccf8f6d129e3626568aa5b9896d8a33c9
nowaylifer <nowaylifer@gmail.com>
2023-12-28 08:54:01 +0300

    fix: make request editor value be changeable via redux action

--- src/components/GraphqlEditor/RequestPanel/RequestEditor.tsx
+++ src/components/GraphqlEditor/RequestPanel/RequestEditor.tsx
@@ -1,21 +1,25 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { useDebouncedCallback } from 'use-debounce';
-import { ReactCodeMirrorRef, ViewUpdate } from '@uiw/react-codemirror';
+import { ReactCodeMirrorRef } from '@uiw/react-codemirror';
 import { updateSchema, graphql } from 'cm6-graphql';
-import { historyField } from '@codemirror/commands';
 import { useAppDispatch, useAppSelector } from '@/utils/hooks/redux-hooks';
-import { changeRequestValue, selectGraphQLSchema } from '@/redux/slices/graphqlSlice';
+import {
+  changeRequestValue,
+  selectGraphQLSchema,
+  selectRequestValue,
+} from '@/redux/slices/graphqlSlice';
 import EditorArea from '@/components/Editor/EditorArea';
 
-const stateFields = { history: historyField };
+/* const stateFields = { history: historyField };
 const storageState = JSON.parse(localStorage.getItem('requestEditorState') ?? 'null');
-const initialState = storageState ? { json: storageState, fields: stateFields } : undefined;
+const initialState = storageState ? { json: storageState, fields: stateFields } : undefined; */
 
 function RequestEditor() {
   const editorAreaRef = useRef<ReactCodeMirrorRef | null>(null);
   const graphqlSchema = useAppSelector(selectGraphQLSchema);
-  const [editorValue, setEditorValue] = useState(storageState?.doc);
-  const langExtension = useRef([graphql(editorValue ? graphqlSchema : undefined)]);
+  const storeValue = useAppSelector(selectRequestValue);
+  const [editorValue, setEditorValue] = useState(storeValue);
+  const langExtensionRef = useRef([graphql(editorValue ? graphqlSchema : undefined)]);
   const dispatch = useAppDispatch();
   const dispatchDebounced = useDebouncedCallback((value: string) => {
     dispatch(changeRequestValue(value));
@@ -27,9 +31,9 @@ function RequestEditor() {
     }
   }, [editorValue, editorAreaRef.current?.view, graphqlSchema]);
 
-  const handleChange = useCallback((value: string, viewUpdate: ViewUpdate) => {
-    const state = viewUpdate.state.toJSON(stateFields);
-    localStorage.setItem('requestEditorState', JSON.stringify(state));
+  const handleChange = useCallback((value: string) => {
+    /* const state = viewUpdate.state.toJSON(stateFields);
+    localStorage.setItem('requestEditorState', JSON.stringify(state)); */
     setEditorValue(value);
     dispatchDebounced(value);
     // eslint-disable-next-line react-hooks/exhaustive-deps -- because of dispatchDebounced
@@ -37,11 +41,11 @@ function RequestEditor() {
 
   return (
     <EditorArea
-      value={storageState?.doc}
-      initialState={initialState}
+      value={storeValue}
+      // initialState={initialState}
       onChange={handleChange}
       ref={editorAreaRef}
-      extensions={langExtension.current}
+      extensions={langExtensionRef.current}
     />
   );
 }
