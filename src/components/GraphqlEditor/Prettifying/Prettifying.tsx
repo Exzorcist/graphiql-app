8c1ae50177e510c067194d522a53302379c513f4
Ivan K <ivan.karpovich.frontend@gmail.com>
2023-12-29 20:52:56 +0300

    feat: add base prettifying logic

--- src/components/GraphqlEditor/Prettifying/Prettifying.tsx
+++ src/components/GraphqlEditor/Prettifying/Prettifying.tsx
@@ -1,37 +1,69 @@
 import { PuzzlePieceIcon } from '@heroicons/react/24/outline';
 import { useSelector, useDispatch } from 'react-redux';
 import { selectRequestValue, changeRequestValue } from '@/redux/slices/graphqlSlice';
+import { IGlobalMessage } from '@/types/Message';
+import { setMessage } from '@/redux/slices/globalMessageSlice';
 
-// import { setLines, combineRows, getRows, trimSpaces, setTabs } from './PrettifyingRules';
-import { setLines, combineRows, getRows, trimSpaces } from './PrettifyingRules';
+import {
+  combineRows,
+  getRows,
+  trimSpaces,
+  arrayStringMutation,
+  setLineBreak,
+  setFieldLine,
+  removeDuplicatedSpaces,
+  removeBraketSpace,
+  addExtraBraketSpace,
+  addExtraArgumentSpace,
+  correctComma,
+  setPadding,
+} from './PrettifyingRules';
+import { useLocalizationContext } from '@/providers/LocalizationProvider';
 
 function Prettifying() {
+  const { t } = useLocalizationContext();
   const dispatch = useDispatch();
   const query = useSelector(selectRequestValue);
 
   const hanldePrettifying = () => {
     let newQuery: string | string[] = '';
-    // console.group();
-    // console.log('Before:');
-    // console.log(query);
-    // console.groupEnd();
 
-    // console.group();
-    // console.log('========================================');
-    // console.log('After:');
+    if (query.includes(';') || query.includes('-') || query.includes('@')) {
+      const wrongSymbolMessage: IGlobalMessage = {
+        type: 'error',
+        text: t.globalMessage.error.prettifying,
+        isShown: true,
+      };
 
-    newQuery = trimSpaces(query);
-    newQuery = getRows(newQuery);
-    newQuery = (newQuery as string[]).map((str) => setLines(str));
-    // newQuery = () => {
-    //     (newQuery as string[]).map((str) => setTabs(str));
-    // }();
-    newQuery = combineRows(newQuery);
+      dispatch(setMessage(wrongSymbolMessage));
+    } else {
+      // console.group();
+      // console.log(query);
+      // console.groupEnd();
 
-    // console.log(newQuery);
-    // console.groupEnd();
+      // console.group();
+      newQuery = trimSpaces(query);
+      newQuery = getRows(newQuery);
 
-    dispatch(changeRequestValue(newQuery));
+      if (Array.isArray(newQuery)) {
+        newQuery = arrayStringMutation(newQuery, trimSpaces);
+        newQuery = arrayStringMutation(newQuery, removeDuplicatedSpaces);
+        newQuery = arrayStringMutation(newQuery, removeBraketSpace);
+        newQuery = arrayStringMutation(newQuery, addExtraBraketSpace);
+        newQuery = arrayStringMutation(newQuery, addExtraArgumentSpace);
+        newQuery = arrayStringMutation(newQuery, correctComma);
+        newQuery = setFieldLine(newQuery);
+        newQuery = setPadding(newQuery);
+        newQuery = arrayStringMutation(newQuery, setLineBreak);
+      }
+
+      newQuery = combineRows(newQuery);
+
+      // console.log(newQuery);
+      // console.groupEnd();
+
+      dispatch(changeRequestValue(newQuery));
+    }
   };
 
   return (
--- src/components/GraphqlEditor/Prettifying/PrettifyingRules.ts
+++ src/components/GraphqlEditor/Prettifying/PrettifyingRules.ts
@@ -1,11 +1,57 @@
-// step 1: Removing spaces at the beginning and end of a line
+// [Help]: Removing spaces at the beginning and end of a line
 export const trimSpaces = (str: string): string => str.trim();
 
-// step 2: Get all rows
+// [Help]: Set line break
+export const setLineBreak = (str: string): string => `${str}\n`;
+
+// [Help]: Clear line removing duplicated spaces
+export const removeDuplicatedSpaces = (str: string): string => str.replace(/\s+/g, ' ');
+
+// [Help]: Clear line removing duplicated spaces
+export const removeBraketSpace = (str: string): string => str.replace(' (', '(');
+
+// [Help]: Add space if need bettween ')' and '{'
+export const addExtraBraketSpace = (str: string): string => str.replace('){', ') {');
+
+// [Help]: Add space if need bettween ':' and argument type
+export const addExtraArgumentSpace = (str: string): string => str.replace(/:\s*/g, ': ');
+
+// [Help]: Add space if need bettween ',' and argument type alse remove where needed
+export const correctComma = (str: string): string => {
+  if (str.includes('(')) {
+    return str.replace(/,\s*/g, ', ');
+  }
+
+  return str.replace(/,/g, '');
+};
+
+// [Help]: Get new array string with callback
+export const arrayStringMutation = (
+  array: string[],
+  callback: (arg: string) => string
+): string[] => {
+  return array.map((str) => callback(str));
+};
+
+// [Help]: Find and set field line
+export const setFieldLine = (array: string[]): string[] => {
+  let newArray: string[] = [];
+
+  array.forEach((str) => {
+    if (str.includes('}')) {
+      newArray = [...newArray, ...str.split(' ')];
+    } else {
+      newArray.push(str);
+    }
+  });
+
+  return newArray;
+};
+
+// [Help]: Get all rows
 export const getRows = (str: string): string[] | string => {
   // Search for strings that end in '{' or '}' characters
-  const regexStep1 = /[^{}]*[{}]\s*/g;
-  const step1 = str.match(regexStep1);
+  const step1 = str.match(/[^{}]*[{}]\s*/g);
 
   if (step1 && step1.length) {
     return step1;
@@ -14,21 +60,32 @@ export const getRows = (str: string): string[] | string => {
   return str;
 };
 
-// step 3: Set rows
-export const setLines = (str: string): string => `${str}\n`;
+export const setPadding = (array: string[]): string[] => {
+  const newArray: string[] = [];
+  let multiplier = 0;
 
-// Step 4: Set tabs
-// export const setTabsMap = (rows: string[]): string[] => {
-//     let count
-// };
+  array.forEach((str) => {
+    if (str.includes('{')) {
+      newArray.push(`${'  '.repeat(multiplier)}${str}`);
+      multiplier += 1;
+    }
 
-export const setTabs = (str: string, multiplier: number): string => {
-  return `${'  '.repeat(multiplier)}${str}`;
+    if (!str.includes('{') && !str.includes('}')) {
+      newArray.push(`${'  '.repeat(multiplier)}${str}`);
+    }
+
+    if (str.includes('}')) {
+      multiplier -= 1;
+      newArray.push(`${'  '.repeat(multiplier)}${str}`);
+    }
+  });
+
+  return newArray;
 };
 
 // final step: combine rows
 export const combineRows = (rows: string[] | string): string => {
-  console.log(rows);
+  // console.log(rows);
   if (Array.isArray(rows)) {
     return rows.join('');
   }
