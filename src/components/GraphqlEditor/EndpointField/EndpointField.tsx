6da8a334a9e31044c756cc4c7405f386151a27ee
nowaylifer <nowaylifer@gmail.com>
2024-01-06 20:16:58 +0300

    feat: implement feature of choosing one of the multiple graphql operations

--- src/components/GraphqlEditor/EndpointField/EndpointField.tsx
+++ src/components/GraphqlEditor/EndpointField/EndpointField.tsx
@@ -5,10 +5,7 @@ import SendRequestButton from './SendRequestButton';
 import { useLocalizationContext } from '@/providers/LocalizationProvider';
 import SchemaButton from './SchemaButton';
 import { PropsWithClassName } from '@/types/PropsWithClassName';
-import {
-  useFetchIntrospectionMutation,
-  useInitRequestMutation,
-} from '@/redux/slices/graphql/graphqlApi';
+import { useFetchIntrospectionMutation } from '@/redux/slices/graphql/graphqlApi';
 import SchemaIndicator from './SchemaIndicator';
 import { useAppDispatch, useAppSelector } from '@/utils/hooks/redux-hooks';
 import { changeEndpointValue, selectEndpointValue } from '@/redux/slices/graphql/graphqlSlice';
@@ -19,11 +16,10 @@ export type EndpointFieldProps = {
 } & PropsWithClassName;
 
 function EndpointField({ onSchemaClick, isSchemaOpen = false, className }: EndpointFieldProps) {
-  const { t } = useLocalizationContext();
   const dispatch = useAppDispatch();
+  const { t } = useLocalizationContext();
   const apiUrl = useAppSelector(selectEndpointValue);
   const [inputValue, setInputValue] = useState(apiUrl);
-  const [initRequest] = useInitRequestMutation();
   const [fetchIntrospection] = useFetchIntrospectionMutation();
 
   const debouncedFetchIntrospection = useDebouncedCallback(fetchIntrospection, 300);
@@ -31,11 +27,6 @@ function EndpointField({ onSchemaClick, isSchemaOpen = false, className }: Endpo
     (value: string) => dispatch(changeEndpointValue(value)),
     300
   );
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    initRequest(inputValue);
-  };
 
   const handleChange = (e: FormEvent<HTMLInputElement>) => {
     const { value } = e.currentTarget;
@@ -45,12 +36,11 @@ function EndpointField({ onSchemaClick, isSchemaOpen = false, className }: Endpo
   };
 
   return (
-    <form
+    <div
       className={cn(
         'h-10 flex w-full bg-editor-secondary border-editor-border border rounded [&:has(input:focus)]:border-editor-accent',
         className
       )}
-      onSubmit={handleSubmit}
     >
       <div className="w-full h-full flex relative">
         <SchemaIndicator className="absolute left-3 absolute-y-center" />
@@ -65,7 +55,7 @@ function EndpointField({ onSchemaClick, isSchemaOpen = false, className }: Endpo
         </div>
       </div>
       <SendRequestButton />
-    </form>
+    </div>
   );
 }
 
--- src/components/GraphqlEditor/EndpointField/SendRequestButton.tsx
+++ src/components/GraphqlEditor/EndpointField/SendRequestButton.tsx
@@ -1,23 +1,25 @@
 import { PlayIcon } from '@heroicons/react/24/outline';
 import { Kind, OperationDefinitionNode } from 'graphql';
+import { useInitRequestMutation } from '@/redux/slices/graphql/graphqlApi';
 import { useLocalizationContext } from '@/providers/LocalizationProvider';
 import { selectRequestAST } from '@/redux/slices/graphql/graphqlSlice';
 import { PropsWithClassName } from '@/types/PropsWithClassName';
 import { useAppSelector } from '@/utils/hooks/redux-hooks';
 import { useBreakpoint } from '@/hooks/useBreakpoint';
 import Button from '@/components/ui/Button';
-import { cn } from '@/utils/cn';
 import {
   DropdownMenu,
   DropdownMenuItem,
   DropdownMenuContent,
   DropdownMenuTrigger,
 } from '@/components/ui/DropdownMenu';
+import { cn } from '@/utils/cn';
 
 function SendRequestButton({ className }: PropsWithClassName) {
   const { t } = useLocalizationContext();
   const isMobileL = useBreakpoint('min-mobile-lg');
   const requestAST = useAppSelector(selectRequestAST);
+  const [initRequest, { isLoading }] = useInitRequestMutation();
 
   const operations = requestAST?.definitions
     .filter((node) => node.kind === Kind.OPERATION_DEFINITION)
@@ -27,39 +29,44 @@ function SendRequestButton({ className }: PropsWithClassName) {
         ? operationNode.name.value
         : `<Unnamed ${operationNode.operation}>`;
 
-      return { label, operationNode };
+      return { label, name: operationNode.name?.value };
     });
+
+  const shouldRenderDropdown = !!operations && operations.length > 1;
 
   const button = (
     <Button
-      type="submit"
       variant="solid"
+      disabled={isLoading}
+      onClick={shouldRenderDropdown ? undefined : () => initRequest()}
       className={cn('px-5 rounded-none rounded-r outline-none', className)}
     >
       {isMobileL ? t.page.editor.sendRequest : <PlayIcon className="w-6 h-6" />}
     </Button>
   );
 
-  return !operations || operations.length === 1 ? (
-    button
-  ) : (
+  return shouldRenderDropdown ? (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>{button}</DropdownMenuTrigger>
+      <DropdownMenuTrigger asChild disabled={isLoading}>
+        {button}
+      </DropdownMenuTrigger>
       <DropdownMenuContent className="bg-editor-primary p-0 border border-editor-border z-10 text-editor-text-color w-[--radix-dropdown-menu-trigger-width] cursor-pointer max-h-[200px] overflow-auto fancy-scrollbar">
         {operations.map((operation, index) => (
           <DropdownMenuItem
+            key={`${operation.label}_${index}`}
+            onClick={() => initRequest(operation.name)}
             className={cn(
               'cursor-pointer hover:bg-editor-secondary py-2 flex justify-center text-center',
               index > 0 && 'border-t border-editor-border'
             )}
-            // eslint-disable-next-line react/no-array-index-key
-            key={`${operation.label}_${index}`}
           >
             {operation.label}
           </DropdownMenuItem>
         ))}
       </DropdownMenuContent>
     </DropdownMenu>
+  ) : (
+    button
   );
 }
 
